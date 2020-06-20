795f2f3c2a752a91e8809c8ef8a32ca92c3d749f
Tama Asrory <tama@tajriy.com>
2020-06-21 06:59:08 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         nuxtjs
       </h1>
       <h2 class="subtitle">
-        My astonishing Nuxt.js project
+        This Example for Nuxt.js project
       </h2>
       <div class="links">
         <a
